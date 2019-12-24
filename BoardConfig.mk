385e6ea024fd73a4781a558c098a9ba0c898cef9
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-24 10:12:37 +0100

    8 OPPS28.65-37-7-12 073f74 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/motorola/montana
+BOARD_VENDOR := motorola
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/motorola/montana/BoardConfigVendor.mk
